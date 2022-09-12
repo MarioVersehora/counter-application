0fb38a93b1c960b283c24a68d47297da61c74097
Dinu Rusu <dinu@codingshadows.com>
2022-09-12 09:47:32 +0300

    counter now works

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 1
+    }
 
-  state = {
-    count: 1
-  }
+    render() {
+        return (
+            <div>
+                <span>{this.state.count}</span>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+            </div>
+        );
+    }
 
-  render() {
-    return (
-      <div>
-        <span>{this.state.count}</span>
-        <button onclick={this.increment()}>Increment</button>
-        <button onclick={this.decrement()}>Decrement</button>
-      </div >
-    );
-  }
+    increment = () => {
+        console.log("incr");
+        this.setState({count: this.state.count + 1});
+    }
 
-  increment() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  decrement() {
-    this.state.count--;
-  }
-
-
+    decrement = () => {
+        this.setState({count: this.state.count - 1});
+    }
 }
 
 export default Counter;
