8a74eb0e4ccdf284622be6069db3ea6cf6936af6
MarioVersehora <marioalbert_o@yahoo.com>
2022-09-12 10:13:37 +0300

    commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,25 +3,25 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
   state = {
-    count: 1
+    count: 0
   }
 
   render() {
     return (
       <div>
         <span>{this.state.count}</span>
-        <button onclick={this.increment()}>Increment</button>
-        <button onclick={this.decrement()}>Decrement</button>
+        <button onclick={this.increment}>Increment</button>
+        <button onclick={this.decrement}>Decrement</button>
       </div >
     );
   }
 
-  increment() {
+  increment = () => {
     this.setState({ count: this.state.count + 1 });
   }
 
-  decrement() {
-    this.state.count--;
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
   }
 
 
